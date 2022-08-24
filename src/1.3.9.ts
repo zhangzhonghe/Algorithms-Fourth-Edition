46f04f42112f7b94e379255c5db7947e7924bea2
被雨水过滤的空气 <958414905@qq.com>
2022-08-24 11:46:41 +0800

    add test

--- src/1.3.9.ts
+++ src/1.3.9.ts
@@ -1,3 +1,5 @@
+import { expect, test } from 'vitest'
+
 /**
  * 题目：编写一段程序，输入一个缺少左括号的表达式
  * 并返回补全括号之后的中序表达式。
@@ -15,18 +17,23 @@
  * ```
  */
 function fixBrackets(str: string): string {
-  const stack: string[] = [];
+  const stack: string[] = []
 
   for (const item of str) {
     if (item === ')') {
       const n2 = stack.pop()
       const operator = stack.pop()
       const n1 = stack.pop()
-      stack.push(`(${n1} ${operator} ${n2})`)
-      continue;
+      stack.push(`(${n1}${operator}${n2})`)
+      continue
     }
-    stack.push(item);
+    stack.push(item)
   }
 
-  return stack.join('');
+  return stack.join('')
 }
+
+// 测试
+test('fixBrackets', () => {
+  expect(fixBrackets('1+2)*3-4)*5-6)))')).toBe('((1+2)*((3-4)*(5-6)))')
+})
